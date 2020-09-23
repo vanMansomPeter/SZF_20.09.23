997f4fdcfd3cb5052259013684e26a48728c274f
kecskegamer2001 <70634456+kecskegamer2001@users.noreply.github.com>
2020-09-23 10:15:08 +0200

    Add project files.

---
+++ SZF_20.09.23/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SZF_20._09._23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
